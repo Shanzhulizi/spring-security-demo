1e2b0fa5fc1717709ccf347489a01b68871d89b8
LMCow <2863993244@qq.com>
2025-05-30 04:00:56 +0800

    一个用来帮我理解SpringSecurity的登录功能实现

---
+++ src/main/java/com/atguigu/securitydemo/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.atguigu.securitydemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+//springsecurity自带的登陆页面
+//    @GetMapping("/")
+//    public String index() {
+//        return "index";
+//    }
+
+}
